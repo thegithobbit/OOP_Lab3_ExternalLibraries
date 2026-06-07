b0154fcb27ffa22507ab18926970594c0c922b0b
thegithobbit <chevikleyla@gmail.com>
2026-06-07 19:25:20 +0300

    Add JSON serialization for transport classes

--- TransportClasses.h
+++ TransportClasses.h
@@ -2,8 +2,13 @@
 #define TRANSPORTCLASSES_H
 
 #include <iostream>
+#include <memory>
 #include <string>
+
+#include <nlohmann/json.hpp>
+
 using namespace std;
+using json = nlohmann::json;
 
 class Transport {
 protected:
@@ -14,6 +19,17 @@ protected:
     double speed;
     double weight;
 
+    json baseJson() const {
+        return {
+            {"type", type},
+            {"name", name},
+            {"number", number},
+            {"fuel", fuel},
+            {"speed", speed},
+            {"weight", weight}
+        };
+    }
+
 public:
     Transport(string t, string n, int num, string f, double s, double w)
         : type(t), name(n), number(num), fuel(f), speed(s), weight(w) {}
@@ -23,9 +39,30 @@ public:
              << ", Fuel: " << fuel << ", Speed: " << speed << ", Weight: " << weight << endl;
     }
 
-    virtual double calculateRange() { return speed * 10; }
-    virtual int getPriority() { return static_cast<int>(speed); }
-    virtual void honk() { cout << name << " honks!\n"; }
+    virtual json toJson() const {
+        return baseJson();
+    }
+
+    string getType() const {
+        return type;
+    }
+
+    string getName() const {
+        return name;
+    }
+
+    virtual double calculateRange() {
+        return speed * 10;
+    }
+
+    virtual int getPriority() {
+        return static_cast<int>(speed);
+    }
+
+    virtual void honk() {
+        cout << name << " honks!\n";
+    }
+
     virtual ~Transport() {}
 };
 
@@ -36,18 +73,28 @@ protected:
 
 public:
     LandTransport(string t, string n, int num, string f, double s, double w, int wh, int p)
-        : Transport(t,n,num,f,s,w), wheels(wh), passengers(p) {}
+        : Transport(t, n, num, f, s, w), wheels(wh), passengers(p) {}
 
     void printInfo() override {
         Transport::printInfo();
         cout << "Wheels: " << wheels << ", Passengers: " << passengers << endl;
     }
 
-    void move() { cout << name << " is moving on the road.\n"; }
+    json toJson() const override {
+        json data = baseJson();
+        data["wheels"] = wheels;
+        data["passengers"] = passengers;
+        return data;
+    }
+
+    void move() {
+        cout << name << " is moving on the road.\n";
+    }
 };
 
 class Car : public LandTransport {
     double enginePower;
+
 public:
     Car(string n, int num, string f, double s, double w, int p, double ep)
         : LandTransport("Car", n, num, f, s, w, 4, p), enginePower(ep) {}
@@ -57,14 +104,29 @@ public:
         cout << "Engine Power: " << enginePower << " HP\n";
     }
 
-    void turboBoost() { speed *= 1.2; cout << name << " activated turbo! Speed: " << speed << endl; }
-    void drift() { cout << name << " is drifting!\n"; }
+    json toJson() const override {
+        json data = LandTransport::toJson();
+        data["enginePower"] = enginePower;
+        return data;
+    }
 
-    int getPriority() override { return static_cast<int>(speed + enginePower/10); }
+    void turboBoost() {
+        speed *= 1.2;
+        cout << name << " activated turbo! Speed: " << speed << endl;
+    }
+
+    void drift() {
+        cout << name << " is drifting!\n";
+    }
+
+    int getPriority() override {
+        return static_cast<int>(speed + enginePower / 10);
+    }
 };
 
 class Bus : public LandTransport {
     double routeLength;
+
 public:
     Bus(string n, int num, string f, double s, double w, int p, double rl)
         : LandTransport("Bus", n, num, f, s, w, 6, p), routeLength(rl) {}
@@ -74,15 +136,29 @@ public:
         cout << "Route Length: " << routeLength << " km\n";
     }
 
-    void openDoors() { cout << name << " doors opened.\n"; }
-    void announceRoute() { cout << name << " announces its route.\n"; }
+    json toJson() const override {
+        json data = LandTransport::toJson();
+        data["routeLength"] = routeLength;
+        return data;
+    }
 
-    int getPriority() override { return passengers; }
+    void openDoors() {
+        cout << name << " doors opened.\n";
+    }
+
+    void announceRoute() {
+        cout << name << " announces its route.\n";
+    }
+
+    int getPriority() override {
+        return passengers;
+    }
 };
 
 class Plane : public Transport {
     double wingspan;
     double maxAltitude;
+
 public:
     Plane(string n, int num, string f, double s, double w, double ws, double ma)
         : Transport("Plane", n, num, f, s, w), wingspan(ws), maxAltitude(ma) {}
@@ -92,15 +168,30 @@ public:
         cout << "Wingspan: " << wingspan << ", Max Altitude: " << maxAltitude << endl;
     }
 
-    void takeOff() { cout << name << " is taking off!\n"; }
-    void land() { cout << name << " is landing!\n"; }
+    json toJson() const override {
+        json data = baseJson();
+        data["wingspan"] = wingspan;
+        data["maxAltitude"] = maxAltitude;
+        return data;
+    }
 
-    int getPriority() override { return static_cast<int>(speed + maxAltitude/1000); }
+    void takeOff() {
+        cout << name << " is taking off!\n";
+    }
+
+    void land() {
+        cout << name << " is landing!\n";
+    }
+
+    int getPriority() override {
+        return static_cast<int>(speed + maxAltitude / 1000);
+    }
 };
 
 class Boat : public Transport {
     double length;
     double draft;
+
 public:
     Boat(string n, int num, string f, double s, double w, double l, double d)
         : Transport("Boat", n, num, f, s, w), length(l), draft(d) {}
@@ -110,14 +201,29 @@ public:
         cout << "Length: " << length << ", Draft: " << draft << endl;
     }
 
-    void dock() { cout << name << " is docking.\n"; }
-    void sail() { cout << name << " is sailing.\n"; }
+    json toJson() const override {
+        json data = baseJson();
+        data["length"] = length;
+        data["draft"] = draft;
+        return data;
+    }
 
-    int getPriority() override { return static_cast<int>(speed + length/10); }
+    void dock() {
+        cout << name << " is docking.\n";
+    }
+
+    void sail() {
+        cout << name << " is sailing.\n";
+    }
+
+    int getPriority() override {
+        return static_cast<int>(speed + length / 10);
+    }
 };
 
 class Truck : public LandTransport {
     double cargoCapacity;
+
 public:
     Truck(string n, int num, string f, double s, double w, int p, double cc)
         : LandTransport("Truck", n, num, f, s, w, 6, p), cargoCapacity(cc) {}
@@ -127,10 +233,23 @@ public:
         cout << "Cargo Capacity: " << cargoCapacity << " tons\n";
     }
 
-    void loadCargo() { cout << name << " loading cargo.\n"; }
-    void unloadCargo() { cout << name << " unloading cargo.\n"; }
+    json toJson() const override {
+        json data = LandTransport::toJson();
+        data["cargoCapacity"] = cargoCapacity;
+        return data;
+    }
 
-    int getPriority() override { return static_cast<int>(cargoCapacity + speed/2); }
+    void loadCargo() {
+        cout << name << " loading cargo.\n";
+    }
+
+    void unloadCargo() {
+        cout << name << " unloading cargo.\n";
+    }
+
+    int getPriority() override {
+        return static_cast<int>(cargoCapacity + speed / 2);
+    }
 };
 
 class Bicycle : public LandTransport {
@@ -138,14 +257,22 @@ public:
     Bicycle(string n, int num, double s, double w)
         : LandTransport("Bicycle", n, num, "none", s, w, 2, 1) {}
 
-    void pedal() { cout << name << " is pedaling.\n"; }
-    void ringBell() { cout << name << " rings the bell!\n"; }
+    void pedal() {
+        cout << name << " is pedaling.\n";
+    }
 
-    int getPriority() override { return static_cast<int>(speed); }
+    void ringBell() {
+        cout << name << " rings the bell!\n";
+    }
+
+    int getPriority() override {
+        return static_cast<int>(speed);
+    }
 };
 
 class Train : public LandTransport {
     int carriages;
+
 public:
     Train(string n, int num, string f, double s, double w, int p, int c)
         : LandTransport("Train", n, num, f, s, w, 8, p), carriages(c) {}
@@ -155,10 +282,110 @@ public:
         cout << "Carriages: " << carriages << endl;
     }
 
-    void depart() { cout << name << " is departing.\n"; }
-    void stop() { cout << name << " stopped.\n"; }
+    json toJson() const override {
+        json data = LandTransport::toJson();
+        data["carriages"] = carriages;
+        return data;
+    }
 
-    int getPriority() override { return passengers + carriages; }
+    void depart() {
+        cout << name << " is departing.\n";
+    }
+
+    void stop() {
+        cout << name << " stopped.\n";
+    }
+
+    int getPriority() override {
+        return passengers + carriages;
+    }
 };
+
+inline shared_ptr<Transport> transportFromJson(const json& data) {
+    const string type = data.at("type").get<string>();
+    const string name = data.at("name").get<string>();
+    const int number = data.at("number").get<int>();
+    const string fuel = data.value("fuel", "none");
+    const double speed = data.at("speed").get<double>();
+    const double weight = data.at("weight").get<double>();
+
+    if (type == "Car") {
+        return make_shared<Car>(
+            name,
+            number,
+            fuel,
+            speed,
+            weight,
+            data.at("passengers").get<int>(),
+            data.at("enginePower").get<double>()
+        );
+    }
+
+    if (type == "Bus") {
+        return make_shared<Bus>(
+            name,
+            number,
+            fuel,
+            speed,
+            weight,
+            data.at("passengers").get<int>(),
+            data.at("routeLength").get<double>()
+        );
+    }
+
+    if (type == "Plane") {
+        return make_shared<Plane>(
+            name,
+            number,
+            fuel,
+            speed,
+            weight,
+            data.at("wingspan").get<double>(),
+            data.at("maxAltitude").get<double>()
+        );
+    }
+
+    if (type == "Boat") {
+        return make_shared<Boat>(
+            name,
+            number,
+            fuel,
+            speed,
+            weight,
+            data.at("length").get<double>(),
+            data.at("draft").get<double>()
+        );
+    }
+
+    if (type == "Truck") {
+        return make_shared<Truck>(
+            name,
+            number,
+            fuel,
+            speed,
+            weight,
+            data.at("passengers").get<int>(),
+            data.at("cargoCapacity").get<double>()
+        );
+    }
+
+    if (type == "Bicycle") {
+        return make_shared<Bicycle>(name, number, speed, weight);
+    }
+
+    if (type == "Train") {
+        return make_shared<Train>(
+            name,
+            number,
+            fuel,
+            speed,
+            weight,
+            data.at("passengers").get<int>(),
+            data.at("carriages").get<int>()
+        );
+    }
+
+    return nullptr;
+}
 
 #endif
